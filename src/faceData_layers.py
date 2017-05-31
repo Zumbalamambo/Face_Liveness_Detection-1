cf508e70f8b96cbd21eeefe2a178de0894cbcef0
yunfan0621 <yunfan@umich.edu>
2017-05-31 21:55:44 +0800

    to finish batch loader and reshape

--- src/faceData_layers.py
+++ src/faceData_layers.py
@@ -2,8 +2,7 @@ import caffe
 
 import numpy as np
 from PIL import Image
-
-import random
+import copy
 
 class FaceDataLayer(caffe.Layer):
 	"""
@@ -29,19 +28,57 @@ class FaceDataLayer(caffe.Layer):
 		self.label_dir = params['label_dir'] 
 		self.split = params['split']
 		self.mean = np.array(params['mean'])
-		self.batchsize = params['batchsize']
+		self.batch_size = params['batch_size']
 		
-		# blob validation
-		if len(top) != 2:
-			raise Exception("Need to define two tops: data and label.")
-		if len(bottom) != 0:
-			raise Exception("Do not define a bottom.")
-
-		# load paths for data and label
+		# load image paths and their labels (both are list of strings)
 		self.image_paths = open(self.data_dir).read().splitlines()
 		self.labels = open(self.label_dir).read().splitlines()
-		
-		# set base index
-		self.idx = 0
+		self.num_samples = len(image_paths)		
 
-	def reshape(self, bottom, top)
+		# set the dataset offset to 0
+		self.offset = 0
+
+	def reshape(self, bottom, top):
+		# called before every forward(), load image and label to net
+		# reshape two top blobs to define data shape. For details, see:
+		# https://github.com/BVLC/caffe/blob/master/examples/pycaffe/layers/pascal_multilabel_datalayers.py
+
+	def forward(self, bottom, top):
+		# assign output for top blob
+		top[0].data[...] = self.data
+		top[1].data[...] = self.label
+
+		# pick next batch, shuffle if necessary
+		# TODO		
+
+	def backward(self, top, propagate_down, bottome):
+		pass
+
+	def shuffle_dataset(self):
+		# shuffle the dataset
+
+		# shuffle the image path and label using the same permutation
+		permute_idx = np.random.permutation(self.num_samples)
+		image_paths_tmp = [self.image_paths[i] for i in permute_idx]
+		labels_tmp = [self.labels[i] for i in permute_idx]
+		
+		self.image_paths = copy.copy(image_paths_tmp)
+		self.labels = copy.copy(labels_tmp)
+
+		# reset the offset
+		self.offset = 0
+
+	def get_next_batch(self):
+		# return the next batch of (image, label) pairs
+		start_offset = self.offset
+		self.offset += self.batch_size
+
+		if self.offset > self.num_samples:
+			# current epoch finished
+			self.shuffle_dataset()
+			start_offset = 0
+			self.offset = batch_size
+		
+		end_offset = self.offset
+
+		
